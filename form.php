6ff3c0ab78c8987cfe5b051c17481ab3e6a841e5
hayya <remhay07@gmial.com>
2023-01-21 13:17:43 +0700

    [PP-4] add new form

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <h1>Form User</h1>\
+    <p>
+        <span>Username</span>
+        <input type="text" name="user">
+    </p>
+    <p>
+        <span>Password</span>
+        <input type="password" name="pass">
+    </p>
+</body>
+</html>
